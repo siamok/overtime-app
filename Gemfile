13521b74829979b5fd658439e174cd1c03865120
Slawek Sz <szlosarczykslawomir@gmail.com>
2022-11-20 14:48:20 +0100

    implement layouts for nav

--- Gemfile
+++ Gemfile
@@ -45,3 +45,4 @@ end
 gem 'devise', '~> 4.8', '>= 4.8.1'
 gem 'bootstrap-sass', '~> 3.4.1'
 gem 'jquery-rails'
+gem 'cssbundling-rails'
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,14 @@
-import './add_jquery'
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import * as bootstrap from "bootstrap"
+import "@popperjs/core"
 
 //= require jquery
-//= require bootstrap-sprockets
+//= require jquery-ui
 //= require jquery_ujs
+//= require popper
 //= require turbolinks
-//= require_tree .
+//= require bootstrap-sprockets
+//= require bootstrap
+//= require_tree .  
