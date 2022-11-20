f0c175eab055c8fe8b165ee938f5f94303f42906
Slawek Sz <szlosarczykslawomir@gmail.com>
2022-11-20 11:38:35 +0100

    post optionally belongs to user for now

--- db/migrate/20221116205702_add_users_to_posts.rb
+++ db/migrate/20221116205702_add_users_to_posts.rb
@@ -1,5 +1,5 @@
 class AddUsersToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :user, null: false, foreign_key: true
+    add_reference :posts, :user, index: true, foreign_key: true, null: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_205702) do
     t.text "rationale"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id", null: true
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
