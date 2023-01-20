e0327b00a680de5e24db92273233c144ce444fec
Slawek Sz <szlosarczykslawomir@gmail.com>
2023-01-20 20:21:22 +0100

    custimize search in admin dashboards

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -10,12 +10,12 @@ class PostDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    id: Field::Number,
-    date: Field::Date,
-    rationale: Field::Text,
-    user: Field::BelongsTo,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime
+    user: Field::BelongsTo.with_options(searchable: false),
+    id: Field::Number.with_options(searchable: false),
+    date: Field::DateTime.with_options(searchable: false),
+    rationale: Field::Text.with_options(searchable: true),
+    created_at: Field::DateTime.with_options(searchable: false),
+    updated_at: Field::DateTime.with_options(searchable: false)
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -24,19 +24,19 @@ class PostDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
+    user
     id
     date
     rationale
-    user
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
+    user
     id
     date
     rationale
-    user
     created_at
     updated_at
   ].freeze
@@ -45,9 +45,9 @@ class PostDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
+    user
     date
     rationale
-    user
   ].freeze
 
   # COLLECTION_FILTERS
