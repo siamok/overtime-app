729013ed0220024712854b4ebac7ec5021f6905d
Slawek Sz <szlosarczykslawomir@gmail.com>
2023-01-20 20:57:14 +0100

    added enum for post status

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_221_116_205_702) do
+ActiveRecord::Schema.define(version: 20_230_120_195_036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20_221_116_205_702) do
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.bigint 'user_id'
+    t.integer 'status', default: 0
     t.index ['user_id'], name: 'index_posts_on_user_id'
   end
 
