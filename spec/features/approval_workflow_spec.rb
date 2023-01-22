a4dc7421d0b04ff5c12e09c5774ba012bdfadc42
Slawek Sz <szlosarczykslawomir@gmail.com>
2023-01-22 19:47:21 +0100

    Non admin cannot see post status

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -12,12 +12,22 @@ describe 'navigate' do
   end
 
   describe 'edit' do
-    it 'has a status that can be edited on the form' do
+    it 'has a status that can be edited on the form by an admin' do
       visit edit_post_path(post)
+
       choose('post_status_approved')
       click_on('Save')
 
       expect(post.reload.status).to eq('approved')
+    end
+
+    it 'cannot be edited by a non admin' do
+      logout(:admin_user)
+      login_as(user, scope: :user)
+
+      visit edit_post_path(post)
+
+      expect(page).to_not have_content('Approved')
     end
   end
 end
