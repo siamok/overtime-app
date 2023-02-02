703ecd555d53ac31181948e2c123c11b7e6bf48a
Slawek Sz <szlosarczykslawomir@gmail.com>
2023-02-02 20:36:35 +0100

    add status column

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 module PostsHelper
+  def status_label(status)
+    status_span_generator(status)
+  end
+
+  private
+
+  def status_span_generator(status)
+    titleize_status = status.titleize
+    case status
+    when 'submitted'
+      content_tag(:span, titleize_status, class: 'label label-primary')
+    when 'approved'
+      content_tag(:span, titleize_status, class: 'label label-success')
+    when 'rejected'
+      content_tag(:span, titleize_status, class: 'label label-danger')
+    end
+  end
 end
