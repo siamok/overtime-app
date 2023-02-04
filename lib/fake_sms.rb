d46e0de58f96ed6288b4db9e1682612fa51672eb
Slawek Sz <szlosarczykslawomir@gmail.com>
2023-02-04 11:26:34 +0100

    created sms stub for tests

---
+++ lib/fake_sms.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module FakeSms
+  Message = Struct.new(:num, :msg)
+  @messages = []
+
+  def self.send_sms(number:, message:)
+    @messages << Message.new(number, message)
+  end
+
+  def self.messages
+    @messages
+  end
+end
