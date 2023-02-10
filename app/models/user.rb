d305f124284d59f5ccc16f9571c8400e5abd7dc2
Slawek Sz <szlosarczykslawomir@gmail.com>
2023-02-10 21:28:06 +0100

    added audit log with model relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   has_many :posts
+  has_many :audit_logs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :audit_logs
   namespace :admin do
     resources :users
     resources :admin_users
