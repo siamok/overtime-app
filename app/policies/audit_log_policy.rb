7ab3e8881e8ecaa606849f22b7cc58207d321885
Slawek Sz <szlosarczykslawomir@gmail.com>
2023-02-10 22:11:05 +0100

    added audit log tab for admins

---
+++ app/policies/audit_log_policy.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class AuditLogPolicy < ApplicationPolicy
+  def index?
+    return true if admin?
+  end
+
+  private
+
+  def admin?
+    admin_types.include?(user.type)
+  end
+end
