2ad5dbf4d5bc0545072ed1843618217875c5155c
Slawek Sz <szlosarczykslawomir@gmail.com>
2023-02-11 16:32:18 +0100

    update audit log dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,26 @@ module ApplicationHelper
   def active?(path)
     'active' if current_page?(path)
   end
+
+  def status_label(status)
+    status_span_generator(status)
+  end
+
+  private
+
+  def status_span_generator(status)
+    titleize_status = status.titleize
+    case status
+    when 'submitted'
+      content_tag(:span, titleize_status, class: 'label label-primary')
+    when 'approved'
+      content_tag(:span, titleize_status, class: 'label label-success')
+    when 'rejected'
+      content_tag(:span, titleize_status, class: 'label label-danger')
+    when 'pending'
+      content_tag(:span, titleize_status, class: 'label label-primary')
+    when 'confirmed'
+      content_tag(:span, titleize_status, class: 'label label-success')
+    end
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,21 +1,5 @@
 # frozen_string_literal: true
 
 module PostsHelper
-  def status_label(status)
-    status_span_generator(status)
-  end
 
-  private
-
-  def status_span_generator(status)
-    titleize_status = status.titleize
-    case status
-    when 'submitted'
-      content_tag(:span, titleize_status, class: 'label label-primary')
-    when 'approved'
-      content_tag(:span, titleize_status, class: 'label label-success')
-    when 'rejected'
-      content_tag(:span, titleize_status, class: 'label label-danger')
-    end
-  end
 end
