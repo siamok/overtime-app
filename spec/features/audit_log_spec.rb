f171e91d0a64a9819f4f9ac9067c228f0d65f4a8
Slawek Sz <szlosarczykslawomir@gmail.com>
2023-02-11 16:10:30 +0100

    implemented view for audit logs

---
+++ spec/features/audit_log_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'AuditLog Feature' do
+  let!(:audit_log) { FactoryBot.create(:audit_log) }
+  let(:admin) { FactoryBot.create(:admin_user) }
+
+  before do
+    login_as(admin, scope: :user)
+  end
+
+  describe 'index' do
+    it 'has an index page that can be reached' do
+      visit audit_logs_path
+
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'renders audit log content' do
+      visit audit_logs_path
+
+      expect(page).to have_content(/TEST, TEST/)
+    end
+  end
+end
