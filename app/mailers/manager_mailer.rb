00ab49945c8137d947242697e0e65e77feccaacb
Slawek Sz <szlosarczykslawomir@gmail.com>
2023-02-12 16:01:09 +0100

    rubofix

--- app/mailers/manager_mailer.rb
+++ app/mailers/manager_mailer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ManagerMailer < ApplicationMailer
   def email(manager)
     @manager = manager
--- spec/mailers/previews/manager_mailer_preview.rb
+++ spec/mailers/previews/manager_mailer_preview.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
+
 # Preview all emails at http://localhost:3000/rails/mailers/manager_mailer
 class ManagerMailerPreview < ActionMailer::Preview
-
 end
