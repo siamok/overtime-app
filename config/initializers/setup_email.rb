13e80b65ba29f94912715939c5f2bbd81e430a74
Slawek Sz <szlosarczykslawomir@gmail.com>
2023-02-22 20:35:13 +0100

    rubofixes

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 ActionMailer::Base.smtp_settings = {
-  :port =>           ENV['SPARKPOST_SMTP_PORT'],
-  :address =>        ENV['SPARKPOST_SMTP_HOST'],
-  :user_name =>      ENV['SPARKPOST_SMTP_USERNAME'],
-  :password =>       ENV['SPARKPOST_SMTP_PASSWORD'],
-  :authentication => :login,
-  :enable_starttls_auto => true
+  port: ENV['SPARKPOST_SMTP_PORT'],
+  address: ENV['SPARKPOST_SMTP_HOST'],
+  user_name: ENV['SPARKPOST_SMTP_USERNAME'],
+  password: ENV['SPARKPOST_SMTP_PASSWORD'],
+  authentication: :login,
+  enable_starttls_auto: true
 }
 ActionMailer::Base.delivery_method = :smtp
